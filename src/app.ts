165bebdcfd51577ae27f8e8530500e93f6e266c8
bosifullstack <bosifullstack@gmail.com>
2021-04-18 11:20:20 -0300

    Clean old data

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import loginRouter from './controllers/login';
-import todoRouter from './controllers/todo';
 import userRouter from './controllers/user';
-import postRouter from './controllers/post';
-import imageRouter from './controllers/image';
-import authorRouter from './controllers/author';
-import mediaRouter from './controllers/media';
-import quoteRouter from './controllers/quote';
 
 import { verifyJWT } from './util/verifyToken';
 import { connectToDataBase } from './mongoConnection';
@@ -25,12 +19,6 @@ app.use(verifyJWT);
 
 app.use(loginRouter);
 app.use(userRouter);
-app.use(todoRouter);
-app.use(postRouter);
-app.use(imageRouter);
-app.use(authorRouter);
-app.use(mediaRouter);
-app.use(quoteRouter);
 
 export const server = app.listen(port);
 
